f05f7fb07750fa8516e9625048b4d2481d3537f8
Warren Haskins <warren@hustleandpraise.com>
2020-08-21 12:23:42 +0100

    package updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,3 @@
-// postcss.config.js
-const purgecss = require("@fullhuman/postcss-purgecss")({
-    // Specify the paths to all of the template files in your project
-    content: ["./partials/*.php", "./*.php", "./templates/*.php"],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
-
 module.exports = {
-    plugins: [
-        require("tailwindcss"),
-        require("autoprefixer"),
-        ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-    ],
+    plugins: [require("tailwindcss"), require("autoprefixer")],
 };
